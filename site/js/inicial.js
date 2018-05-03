7b8a8f0c0d482373f7caecb967227e896e7d1c8d
Aluno <Aluno@Digital.net>
2018-05-03 13:36:54 -0300

    Site puro em HTML, CSS e JS

---
+++ site/js/inicial.js
@@ -0,0 +1,19 @@
+function abrirFrame(id){
+	var tipo = document.getElementById(id).id;
+	
+	if(tipo == 'chocadeira'){
+		document.getElementById('frame').src = "../area_chocadeira/cadastro.html";
+	}
+	if(tipo == 'ovo'){
+		document.getElementById('frame').src = "../area_ovo/cadastro.html";
+	}
+	if(tipo == 'dispositivo'){
+		document.getElementById('frame').src = "../area_chocadeira/dispositivo.html";
+	}
+	if(tipo == 'grafico'){
+		document.getElementById('frame').src = "../area_chocadeira/grafico.html";
+	}
+	if(tipo == 'logout'){
+		confirm('Tem certeza que deseja sair?');
+	}
+}
