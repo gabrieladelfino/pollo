52191097900eb104c825ea746e92b015a14f5d29
LiviaSayuri <livia.sayuri@hotmail.com>
2018-05-12 15:47:17 -0300

    Asp Cadatrar n funciona ainda

---
+++ Asp/Pollo/Pollo/index.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Pollo
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCadastrar_Click(object sender, EventArgs e)
+        {
+
+            Server.Transfer("/area_usuario/cadastro.aspx", true); 
+            Server.Transfer("index.aspx", false);
+        }
+    }
+}
