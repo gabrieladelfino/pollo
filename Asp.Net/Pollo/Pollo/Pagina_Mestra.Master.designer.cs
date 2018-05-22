c83df1d40968d5d05843a6343bb2935f317df04d
Aluno <Aluno@Digital.net>
2018-05-22 12:52:24 -0300

    Label

--- Asp.Net/Pollo/Pollo/Pagina_Mestra.Master.designer.cs
+++ Asp.Net/Pollo/Pollo/Pagina_Mestra.Master.designer.cs
@@ -31,6 +31,15 @@ namespace Pollo {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// Controle label.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label label;
+        
+        /// <summary>
         /// Controle btnSair.
         /// </summary>
         /// <remarks>
